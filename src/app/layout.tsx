df9468924ad254028f66b85d973685a910670e5e
Lewis Rye <lewisryework@gmail.com>
2025-05-16 14:30:28 +0100

    Allow tauri build script
    Co-Authored-By: Koushic <149502679+KoushicSumathiKumar@users.noreply.github.com>

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type React from "react";
+import type { Metadata } from "next";
+import { Raleway } from "next/font/google";
+import "./globals.css";
+
+const raleway = Raleway({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TKL Snap",
+  description: "TKL SNAP APP COMING SOON",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={raleway.className}>{children}</body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,25 @@
+"use client";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
   async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>Welcome to TKL Snap</h1>
 
       <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
+        <a href="https://tauri.app" target="_blank" rel="noreferrer">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <p>Click on the logos to learn more.</p>
 
       <form
         className="row"
